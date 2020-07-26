01e05e44cb8f2d032c0678788de8d1dba1ff5f4c
hyq <422565155@qq.com>
2020-07-26 23:04:40 +0800

    yy

---
+++ src/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: process.env.VUE_APP_ENV_MODE,
+  local: { // 本地测试环境配置
+    url: {
+      baseURL: '/', // 接口域名地址
+      timeout: 60000
+    }
+  },
+  dev: {// 开发联测环境配置
+    url: {
+      baseURL: 'http://api.leizikeji.top/',
+      // baseURL: 'http://plane.leizikeji.top/',
+      timeout: 60000
+    }
+  },
+  prod: {// 生产环境配置
+    url: {
+      // baseURL: 'http://api.leizikeji.top/',
+      baseURL: window.$requestHttp,
+      // baseURL: 'http://plane.leizikeji.top/',
+      timeout: 60000 // 超时时间
+    }
+  }
+}
+
